4191b95cb4fd60af3eab7df22c79205164d73af2
Zuhri Nurhuda <zuhri.nurhuda@gmail.com>
2017-09-19 20:42:44 +0700

    binary search done

--- binary.js
+++ binary.js
@@ -1,16 +1,38 @@
 'use strict'
 
-var testArrayGenap = [40, 18, 22, 32, 90, 10, 10, 22, 8]
-var testArrayGanjil = [3, 31, 89, 53, 53, 85, 77, 21, 55]
+var testArrayGenap = [40, 18, 22, 32, 90, 10, 10, 22, 8];
+var testArrayGanjil = [3, 31, 89, 53, 53, 85, 77, 21, 55];
 
 function ownSort(arr) {
   // Your sorting code
-  return arr
+  for (var i = 1; i < arr.length; i++) {
+    for (var j = 0; j < arr.length; j++) {
+      if (arr[i] < arr[j]) {
+        let tampung = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tampung;
+      }
+    }
+  }
+  return arr;
 }
 
-function binarySearch (search, array) {
+function binary_search (search, array) {
   // Your searching code
-  return 0;
+  let awal = 0;
+  let akhir = array.length;
+
+  while (awal <= akhir) {
+    let tengah = Math.floor((awal + akhir) / 2);
+    if (array[tengah] == search) {
+      return tengah;
+    } else if (array[tengah] < search) {
+      awal = tengah + 1;
+    } else if (array[tengah] > search) {
+      akhir = tengah - 1;
+    }
+  }
+  return -1;
 }
 
 var arrayGenapSorted = ownSort(testArrayGenap)
