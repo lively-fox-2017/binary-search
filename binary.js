e1ab3875b78ca05924c0df02c977c903bd24e29a
febrimailand <febri.mailand@gmail.com>
2017-09-19 17:05:19 +0700

    challenge binary-search

--- binary.js
+++ binary.js
@@ -5,26 +5,53 @@ var testArrayGanjil = [3, 31, 89, 53, 53, 85, 77, 21, 55]
 
 function ownSort(arr) {
   // Your sorting code
+  for (var i =0; i < arr.length; i++){
+    for(var j = 0; j<i; j++){
+      if(arr[j] > arr[i]){
+        var tampung = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tampung;
+      }
+    }
+  }
   return arr
 }
 
 function binarySearch (search, array) {
   // Your searching code
-  return 0;
+  var lo = 0;
+  var hi = array.length-1;
+  var middle = 0;
+
+  while(lo<=hi){
+    middle = Math.floor(lo+(hi-lo)/2);
+    if (search < array[middle]){
+      hi = middle -1;
+    } else if (search > array[middle]){
+      lo = middle +1;
+    } else {
+      return middle;
+    }
+  }
+
+  return -1;
 }
 
 var arrayGenapSorted = ownSort(testArrayGenap)
 var arrayGanjilSorted = ownSort(testArrayGanjil)
 
-// Driver code
-console.log(binary_search(8, arrayGenapSorted))
-console.log(binary_search(10, arrayGenapSorted))
-console.log(binary_search(33, arrayGenapSorted))
+//arrayGenapSorted = [8, 10, 10, 18, 22, 22, 32, 40, 90]
+//arrayGanjilSorted = [3, 21, 31, 53, 53, 55, 77, 85, 89]
 
-console.log(binary_search(53, arrayGanjilSorted))
-console.log(binary_search(3, arrayGanjilSorted))
-console.log(binary_search(2, arrayGanjilSorted))
+// Driver code
+console.log(binarySearch(8, arrayGenapSorted)) // 0
+console.log(binarySearch(10, arrayGenapSorted)) // 1
+console.log(binarySearch(33, arrayGenapSorted)) // -1
+
+console.log(binarySearch(53, arrayGanjilSorted)) // 4
+console.log(binarySearch(3, arrayGanjilSorted)) // 0
+console.log(binarySearch(2, arrayGanjilSorted)) // -1
 
 module.exports = {
-  binary_search
+  binarySearch
 }
