7f6d70ec9244fad8ae23b6f2b802fed62476629a
chandrabuwana <cbuwana@gmail.com>
2017-09-19 18:21:44 +0700

    binary search use WHILE

--- binary.js
+++ binary.js
@@ -5,16 +5,46 @@ var testArrayGanjil = [3, 31, 89, 53, 53, 85, 77, 21, 55]
 
 function ownSort(arr) {
   // Your sorting code
+  let temp=0
+  for(var i=0;i<arr.length;i++){
+    for(var j=0;j<i;j++){//cek dengan sebelahnya makanya 1
+      if(arr[i]<arr[j]){
+        temp= arr[i]
+        arr[i]=arr[j]
+        arr[j]=temp
+      }
+    }
+  }
   return arr
 }
+// console.log(ownSort());
+// console.log(ownSort());
 
-function binarySearch (search, array) {
+
+function binary_search (search, array) {
   // Your searching code
-  return 0;
+  let low=0,
+      hi=array.length-1,
+      mid=0
+  while(low<=hi){
+    mid=(low+(hi-low)/2)
+    mid=Math.floor(mid)
+    if(search<array[mid]){
+      hi=mid-1
+    }else if(search>array[mid]){
+      low=mid+1
+    }else{
+      return mid
+    }
+  }
+  return -1;
 }
 
 var arrayGenapSorted = ownSort(testArrayGenap)
 var arrayGanjilSorted = ownSort(testArrayGanjil)
+
+console.log(arrayGenapSorted);
+console.log(arrayGanjilSorted);
 
 // Driver code
 console.log(binary_search(8, arrayGenapSorted))
