74ff271feceb1fecfa7fbb42bd56d25335b502d7
vbagustinus <vbagustinus@gmail.com>
2017-09-19 20:27:12 +0700

    done

--- binary.js
+++ binary.js
@@ -1,20 +1,42 @@
 'use strict'
 
-var testArrayGenap = [40, 18, 22, 32, 90, 10, 10, 22, 8]
-var testArrayGanjil = [3, 31, 89, 53, 53, 85, 77, 21, 55]
+var testArrayGenap = [40, 18, 22, 32, 90, 10, 10, 22, 8];
+var testArrayGanjil = [3, 31, 89, 53, 53, 85, 77, 21, 55];
 
 function ownSort(arr) {
-  // Your sorting code
-  return arr
+  for (var a = 1; a < arr.length; a++) {
+    for (var b = 0; b < a - 1; b++) {
+      if (arr[a] < arr[b]) {
+        let simpan = arr[b];
+        arr[b] = arr[a];
+        arr[a] = simpan;
+      }
+    }
+  }
+  return arr;
 }
 
-function binarySearch (search, array) {
-  // Your searching code
-  return 0;
+function binary_search(search, array) {
+  console.log(array.length);
+  let mulaiIndex = 0;
+  let highIndex = array.length - 1;
+  let tengahIndex = Math.floor((mulaiIndex + highIndex) / 2);
+  while (highIndex >= mulaiIndex) {
+    if (array[tengahIndex] === search) {
+      return tengahIndex;
+    } else if (search < array[tengahIndex]) {
+      highIndex = tengahIndex - 1;
+
+    } else if (search > array[tengahIndex]) {
+      mulaiIndex = tengahIndex + 1;
+    }
+    tengahIndex = Math.floor((mulaiIndex + highIndex) / 2);
+  }
+  return -1;
 }
 
-var arrayGenapSorted = ownSort(testArrayGenap)
-var arrayGanjilSorted = ownSort(testArrayGanjil)
+var arrayGenapSorted = ownSort(testArrayGenap);
+var arrayGanjilSorted = ownSort(testArrayGanjil);
 
 // Driver code
 console.log(binary_search(8, arrayGenapSorted))
@@ -27,4 +49,4 @@ console.log(binary_search(2, arrayGanjilSorted))
 
 module.exports = {
   binary_search
-}
+};
