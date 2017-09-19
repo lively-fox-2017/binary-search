bb73e33bcb004f89969587a4a85332c1ea489f2e
= <pangkeyd@gmail.com>
2017-09-19 17:02:50 +0700

    binarySearch-dpangkey

--- binary.js
+++ binary.js
@@ -5,26 +5,50 @@ var testArrayGanjil = [3, 31, 89, 53, 53, 85, 77, 21, 55]
 
 function ownSort(arr) {
   // Your sorting code
+  var temp = 0
+  for(var i = 0; i < arr.length; i++){
+    for(var j = 0; j < i; j++){
+      if(arr[i] < arr[j]){
+        temp = arr[i]
+        arr[i] = arr[j]
+        arr[j] = temp
+      }
+    }
+  }
   return arr
 }
 
-function binarySearch (search, array) {
+// [ 8, 10, 10, 18, 22, 22, 32, 40, 90 ]
+// [ 3, 21, 31, 53, 53, 55, 77, 85, 89 ]
+
+// ========== PSEUDOCODE ==========
+// => variabel array
+// => lakukan perulangan dengan variabel array tersebut
+// => kondisikan jika parameter search(atau angka yang ingin kita cari) !== -1
+// => kembalikan nilai index perulangan tersebut
+
+function binarySearch(search, array) {
   // Your searching code
-  return 0;
+  for(var i = 0; i < array.length; i++){
+    if(array[i] === search){
+      return i
+    }
+  }
+  return -1;
+
 }
 
 var arrayGenapSorted = ownSort(testArrayGenap)
 var arrayGanjilSorted = ownSort(testArrayGanjil)
 
+console.log(ownSort(arrayGenapSorted))
+console.log(ownSort(arrayGanjilSorted))
+
 // Driver code
-console.log(binary_search(8, arrayGenapSorted))
-console.log(binary_search(10, arrayGenapSorted))
-console.log(binary_search(33, arrayGenapSorted))
+console.log(binarySearch(8, arrayGenapSorted))
+console.log(binarySearch(10, arrayGenapSorted))
+console.log(binarySearch(33, arrayGenapSorted))
 
-console.log(binary_search(53, arrayGanjilSorted))
-console.log(binary_search(3, arrayGanjilSorted))
-console.log(binary_search(2, arrayGanjilSorted))
-
-module.exports = {
-  binary_search
-}
+console.log(binarySearch(53, arrayGanjilSorted))
+console.log(binarySearch(3, arrayGanjilSorted))
+console.log(binarySearch(2, arrayGanjilSorted))
