a3fa9b6e9cf5ff2f0c590e997d0395b526c053a9
jasoneffendi <jasoneffendi27@gmail.com>
2017-09-19 17:51:50 +0700

    Tidak mengembalikan index

--- binary.js
+++ binary.js
@@ -3,19 +3,6 @@
 var testArrayGenap = [40, 18, 22, 32, 90, 10, 10, 22, 8]
 var testArrayGanjil = [3, 31, 89, 53, 53, 85, 77, 21, 55]
 
-// function ownSort(arr) {
-//   for (var i =  0; i <= arr.length - 1; i++) {
-//     for (var j = 0; j <= i; j++) {
-//       if (arr[j + 1] < arr[j]) {
-//         var temp = arr[j];
-//         arr[j] = arr[j + 1];
-//         arr[j + 1] = temp;
-//       }
-//     }
-//   }
-//   return arr;
-// }
-
 function ownSort(arr){
   var swap;
   var swapCount = 0;
@@ -35,20 +22,25 @@ function ownSort(arr){
 
 function binarySearch (search, array) {
     // Your searching code
-    var guess;
-    var min = 0;
-    var max = array.length - 1;
-
-    while(min <= max){
-      guess = Math.floor((min + max) /2);
-	     if(array[guess] === search)
-	      return guess;
-	      else if(array[guess] < search)
-	        min = guess + 1;
-	      else
-	        max = guess - 1;
-     }
-     return -1;
+    if(array.length == 1 && array[0] !== search) {
+      return -1
+    }
+    if(array.length == 1 ) {
+      return array[0]
+    }
+    if(array[Math.floor(array.length/2)] == search) {
+      return array[Math.floor(array.length/2)]
+    } else if (array[0] === array [1] && array[0] === search) {
+      return array[0]
+    } else if(search < array[Math.floor(array.length/2)]) {
+      array.splice(Math.floor(array.length/2))
+      console.log(array)
+      return binarySearch(search, array)
+    } else if (search > array[Math.floor(array.length/2)]) {
+      array.splice(0,Math.floor(array.length/2))
+      console.log(array)
+      return binarySearch(search,array)
+    }
 }
 
 var arrayGenapSorted = ownSort(testArrayGenap)
@@ -56,11 +48,11 @@ var arrayGanjilSorted = ownSort(testArrayGanjil)
 // console.log(arrayGenapSorted)
 
 // Driver code
-console.log(binarySearch(8, arrayGenapSorted))
+// console.log(binarySearch(8, arrayGenapSorted))
 console.log(binarySearch(10, arrayGenapSorted))
-// console.log(binarySearch(33, arrayGenapSorted))
+console.log(binarySearch(33, arrayGenapSorted))
 
-// console.log(binarySearch(53, arrayGanjilSorted))
+console.log(binarySearch(53, arrayGanjilSorted))
 // console.log(binarySearch(3, arrayGanjilSorted))
 // console.log(binarySearch(2, arrayGanjilSorted))
 
