62de4f8ae884221f5e8e541670121da5447c433c
chandrabuwana <cbuwana@gmail.com>
2017-09-19 20:39:54 +0700

    binary search use recursion

--- binary.js
+++ binary.js
@@ -21,23 +21,37 @@ function ownSort(arr) {
 // console.log(ownSort());
 
 
-function binary_search (search, array) {
-  // Your searching code
-  let low=0,
-      hi=array.length-1,
-      mid=0
-  while(low<=hi){
-    mid=(low+(hi-low)/2)
-    mid=Math.floor(mid)
-    if(search<array[mid]){
-      hi=mid-1
-    }else if(search>array[mid]){
-      low=mid+1
-    }else{
-      return mid
-    }
+// function binary_search (search, array) {
+//   // Your searching code
+//   let low=0,
+//       hi=array.length-1,
+//       mid=0
+//   while(low<=hi){
+//     mid=(low+(hi-low)/2)
+//     mid=Math.floor(mid)
+//     if(search<array[mid]){
+//       hi=mid-1
+//     }else if(search>array[mid]){
+//       low=mid+1
+//     }else{
+//       return mid
+//     }
+//   }
+//   return -1;
+// }
+
+function binary_search(search,array,low,hi){
+  let mid=Math.floor((low+hi)/2);
+  if(low>=hi && search !== array[mid]){
+    return -1
+  }else if(search===array[mid]){
+    // console.log(mid);
+    return mid
+  }else if(search<array[mid]){
+    return binary_search(search,array,low,mid -1)
+  }else if(search> array[mid]){
+    return binary_search(search,array,mid+1,hi)
   }
-  return -1;
 }
 
 var arrayGenapSorted = ownSort(testArrayGenap)
@@ -47,13 +61,13 @@ console.log(arrayGenapSorted);
 console.log(arrayGanjilSorted);
 
 // Driver code
-console.log(binary_search(8, arrayGenapSorted))
-console.log(binary_search(10, arrayGenapSorted))
-console.log(binary_search(33, arrayGenapSorted))
+console.log(binary_search(8, arrayGenapSorted,0,arrayGenapSorted.length-1))
+console.log(binary_search(10, arrayGenapSorted,0,arrayGenapSorted.length-1))
+console.log(binary_search(33, arrayGenapSorted,0,arrayGenapSorted.length-1))
 
-console.log(binary_search(53, arrayGanjilSorted))
-console.log(binary_search(3, arrayGanjilSorted))
-console.log(binary_search(2, arrayGanjilSorted))
+console.log(binary_search(53, arrayGanjilSorted,0,arrayGanjilSorted.length-1))
+console.log(binary_search(3, arrayGanjilSorted,0,arrayGanjilSorted.length-1))
+console.log(binary_search(2, arrayGanjilSorted,0,arrayGanjilSorted.length-1))
 
 module.exports = {
   binary_search
